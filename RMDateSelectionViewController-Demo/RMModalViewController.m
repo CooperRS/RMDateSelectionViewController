e28f572f61710be4535a695d1add7f16f27cacb9
CooperRS <CooperRS@yasit.net>
2013-11-03 20:30:16 +0100

    Removed unused demo code

--- RMDateSelectionViewController-Demo/RMModalViewController.m
+++ RMDateSelectionViewController-Demo/RMModalViewController.m
@@ -14,18 +14,6 @@
 
 @implementation RMModalViewController
 
-#pragma mark - Actions
-- (IBAction)openDateSelectionController:(id)sender {
-    RMDateSelectionViewController *dateSelectionVC = [RMDateSelectionViewController dateSelectionController];
-    dateSelectionVC.delegate = self;
-    [dateSelectionVC show];
-    
-    //After -[RMDateSelectionViewController show] or -[RMDateSelectionViewController showFromViewController:] has been called you can access the actual UIDatePicker via the datePicker property
-    dateSelectionVC.datePicker.datePickerMode = UIDatePickerModeDateAndTime;
-    dateSelectionVC.datePicker.minuteInterval = 5;
-    dateSelectionVC.datePicker.date = [NSDate dateWithTimeIntervalSinceReferenceDate:0];
-}
-
 - (IBAction)doneButtonTapped:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
