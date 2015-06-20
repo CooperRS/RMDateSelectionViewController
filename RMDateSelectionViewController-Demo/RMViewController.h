46a93cbb9fa1d4632955826b35a66807f3a47564
CooperRS <cooperrs@gmail.com>
2015-06-20 23:44:15 +0200

    Updated copyright info

--- RMDateSelectionViewController-Demo/RMViewController.h
+++ RMDateSelectionViewController-Demo/RMViewController.h
@@ -3,7 +3,7 @@
 //  RMDateSelectionViewController-Demo
 //
 //  Created by Roland Moers on 26.10.13.
-//  Copyright (c) 2013 Roland Moers
+//  Copyright (c) 2013-2015 Roland Moers
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
